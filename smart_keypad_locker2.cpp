6029ee56e4599d8983432de655d82f98292f49f9
Abhay Kumar <154926912+abhayarya91@users.noreply.github.com>
2024-09-20 17:18:08 +0530

    Add files via upload
    I built a smart keypad locker using Arduino and ESP 8832 in C++ and I will add more features to it in the future.

---
+++ smart_keypad_locker2.cpp
@@ -0,0 +1,10 @@
+
+
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Cyberhunter"   // replace MySSID with your WiFi network name
+#define SECRET_PASS "217525141" // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID  2323175   // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "JKQBD0ZNN1Y8UTP2"   // replace XYZ with your channel write API Key
